1d65a555e6ca9382196143f2fdbc98efaf931e3e
fmele-pixel <fmele@udesa.edu.ar>
2026-06-15 18:21:00 -0300

    profile

--- src/screen/Profile/Profile.js
+++ src/screen/Profile/Profile.js
@@ -1,16 +1,79 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
-
+import { Text, View, Pressable, StyleSheet, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import { auth, db } from "../../firebase/config";
+import Posteo from "../../componentes/posteo";
 export default function Profile({ navigation }) {
+
+  const [posteo, setPosteo] = useState([]);
+  const [nombre, setNombre] = useState("");
+
+  function logout() {
+    auth.signOut()
+      .then(() => {
+        navigation.navigate("Login");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    db.collection("posts")
+      .where("owner", "==", auth.currentUser.email)
+      .onSnapshot(docs => {
+
+        let posteos = [];
+
+        docs.forEach(doc => {
+          posteos.push({
+            id: doc.id,
+            posteoUsuario: doc.data()
+          });
+        });
+
+        setPosteo(posteos);
+      });
+  }, []);
+
+  useEffect(() => {
+    db.collection("users")
+      .where("owner", "==", auth.currentUser.email)
+      .onSnapshot(docs => {
+        docs.forEach(doc => {
+          setNombre(doc.data().name);
+        });
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
+
       <Text style={styles.title}>Mi Perfil</Text>
+
+      <Text>{nombre}</Text>
+      <Text>{auth.currentUser.email}</Text>
+
+      <FlatList
+        data={posteo}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <Posteo
+            id={item.id}
+            posteoUsu={item.posteoUsuario}
+            navigation={navigation}
+          />
+        )}
+      />
 
       <Pressable
         style={styles.button}
-        onPress={() => navigation.navigate("Login")}
+        onPress={logout}
       >
-        <Text style={styles.textButton}>Desloguearse</Text>
+        <Text style={styles.textButton}>
+          Desloguearse
+        </Text>
       </Pressable>
+
     </View>
   );
 }
@@ -36,3 +99,4 @@ const styles = StyleSheet.create({
     color:"#fff"
   }
 });
+
